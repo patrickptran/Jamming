d357373a1e5c768763d00a2607ea518875b95fb7
MangTran <83230443+MangTran@users.noreply.github.com>
2023-02-15 13:58:56 +0100

    first commit - pratice projet - codecademy

---
+++ src/Components/App/App.js
@@ -0,0 +1,98 @@
+
+import React from 'react';
+
+import './App.css';
+import Playlist from "../Playlist/Playlist";
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+
+// SearchBar  SearchResults
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchResults: [{ name: 'name1', artist: 'artist1', album: 'album1', id: 1 },
+      { name: 'name2', artist: 'artist2', album: 'album2', id: 2 },
+      { name: 'name3', artist: 'artist3', album: 'album3', id: 3 }
+      ],
+      playlistName: "My Playlist",
+      playlistTracks: [{ name: 'playListName1', artist: 'artist track1', album: 'album track1', id: 4 },
+      { name: 'playListName2', artist: 'artist track2', album: 'album track2', id: 5 },
+      { name: 'playListName3', artist: 'artist track3', album: 'album track3', id: 6 }],
+    };
+    this.addTrack = this.addTrack.bind(this);
+    this.removeTrack = this.removeTrack.bind(this);
+    this.updatePlaylistName = this.updatePlaylistName.bind(this);
+    this.savePlaylist = this.savePlaylist.bind(this);
+    this.search = this.search.bind(this);
+  }
+
+  addTrack(track) {
+    let tracks = this.state.playlistTracks;
+    if (tracks.find(savedTrack => savedTrack.id === track.id)) {
+      return;
+    }
+
+    tracks.push(track);
+    this.setState({ playlistTracks: tracks });
+  }
+
+  removeTrack(track) {
+    let tracks = this.state.playlistTracks;
+    tracks = tracks.filter(currentTrack => currentTrack.id !== track.id);
+
+    this.setState({ playlistTracks: tracks });
+  }
+
+  updatePlaylistName(name) {
+    this.setState({ playlistName: { name } });
+  }
+
+  savePlaylist() {
+    //alert("That button is working correctly");
+    const trackURIs = this.state.playlistTracks.map(track => track.uri);
+    // save the user's playlist and their account 
+  }
+
+  search(term) {
+    console.log(term);
+
+  }
+
+  //get a Spotify user's access token 
+  //send a search request to the Spotify API
+  //save a user's playlist to their Spotify account
+
+
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className="highlight">mmm</span>ing</h1>
+        <div className="App">
+
+          <SearchBar onSearch={this.search} />
+
+          <div className="App-playlist">
+
+            <SearchResults
+              searchResults={this.state.searchResults}
+              onAdd={this.addTrack} />
+
+            <Playlist
+              playlistName={this.state.playlistName}
+              playlistTracks={this.state.playlistTracks}
+              onRemove={this.removeTrack}
+              onNameChange={this.updatePlaylistName}
+              onSave={this.savePlaylist}
+            />
+
+          </div>
+        </div>
+      </div>
+    );
+
+  }
+}
+
+export default App;
